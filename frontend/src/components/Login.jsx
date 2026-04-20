76ff8c08b710514ecb879f9e02a59a6f1d79eee4
Prakash Mishra <pm2466599@gmail.com>
2026-04-21 00:13:38 +0530

    frontend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../utils/axios";
-import "../styles/login.css";
+import "../styles/Login.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
