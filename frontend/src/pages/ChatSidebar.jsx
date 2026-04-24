6c7063b84517f6d2d03aae6d60fd6ea04a5869fa
Prakash Mishra <pm2466599@gmail.com>
2026-04-24 05:41:58 +0530

    k

--- frontend/src/pages/ChatSidebar.jsx
+++ frontend/src/pages/ChatSidebar.jsx
@@ -30,7 +30,7 @@ export default function ChatSidebar({
         {conversations?.map((c) => {
           
           // 🔥 FULL SAFETY
-          if (!c || !c.members) return null;
+    if (!c || !Array.isArray(c.members)) return null;
 
           const otherUser = c.members.find(
             (m) =>
